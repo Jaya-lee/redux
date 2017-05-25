aaff436341da35f78e8badbbc3d6ee8cae477a5a
Jaya-lee <18716039175@163.com>
2017-05-25 21:16:26 +0800

    redux完美使用

---
+++ src2/Search.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import store from './redux/store'
+class CommentBox extends Component {
+    constructor(){
+        super();
+
+        this.handleSubmit=this.handleSubmit.bind(this)
+        this.handleInput=this.handleInput.bind(this)
+    }
+    state={
+        input:''
+    }
+
+    handleInput(e){
+        this.setState({
+            input:e.target.value
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        e.target.reset()
+        if(this.state.input.trim()){
+            //发出action
+            store.dispatch({type:'ADD_COMMENT',comment:this.state.input})
+
+            this.setState({
+                input:''
+            })
+
+            // return this.props.num(this.state.comment.length+1)
+        }
+
+    }
+  render() {
+    return (
+      <div className='comment-box'>
+          <div className='search'>
+              <ul>
+                 {this.props.comments.map((item,index) =>
+                 <li className='comment' key={Math.random()}
+                >{item}</li>
+            )}
+              </ul>
+              {/* <li className='comment' key={Math.random()}>{comments}</li> */}
+
+              <form className='comment-form' onSubmit={this.handleSubmit}>
+                  <input onChange={this.handleInput}
+                      value={this.state.input}
+                      className='input'></input>
+                  <button className='submit-btn'>提交</button>
+              </form>
+              <div className='underline'></div>
+          </div>
+      </div>
+    )
+  }
+}
+const mapStateToProps=(state)=>({
+    comments:state.comments
+})
+export default connect(mapStateToProps)(CommentBox)
---
+++ src2/finish.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Finish extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default Finish
